bf3bc13d71085c8adf0e8d623534ad02d6717dc5
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-10-26 12:58:39 +0700

    ubah nama database

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$koneksi =  mysqli_connect('localhost','root','','crud_skinrate');
+$koneksi =  mysqli_connect('localhost','root','','crud_toko');
 
 if (!$koneksi) {
 	echo "Koneksi anda gagal";
