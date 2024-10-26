d43951e49d5e9ea088f2ddfb2df9def327bed003
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-10-26 16:22:45 +0700

    commit

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
     <section id="login" class="pt-5 ">
         <div class="container mt-5 justify-content-center col-md-4  col-lg-3  justify-content-between card">
             <div class="mb-3 ">
-                <form action="barang.php" method="post">
+                <form action="proses_login.php" method="post">
                     <h2 class="text-center">Form Login</h2>
 
                     <label for="username" class="form-label">Username: </label>
-                    <input type="username" class="form-control" id="username">
+                    <input type="username" class="form-control" id="username" name="username">
 
                     <label for="password" class="form-label">Password: </label>
-                    <input type="password" class="form-control" id="password"><br>
+                    <input type="password" class="form-control" id="password" name="password"><br>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
--- proses_login.php
+++ proses_login.php
@@ -1,18 +1,20 @@
-<?php 
+<?php
 session_start();
 include("koneksi.php");
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$sql = "SELECT * FORM users WHERE username='$username' AND password='$password'";
-$result = $conn->query($sql);
+$sql = "SELECT * from user WHERE username='$username' AND password='$password'";
+$result = $koneksi->query($sql);
 
 if ($result->num_rows > 0) {
     $_SESSION['username'] = $username;
-    header("Location: welcome.php");
+    header("Location: dashboard.php");
 } else {
     echo "Login gagal. <a href='index.php'>Login Gagal!</a>";
 }
 
-$conn->close();
+$koneksi->close();
 ?>
+
